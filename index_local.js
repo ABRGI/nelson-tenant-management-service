b5c844913075b216ad7e9bf6b808734b58061ec8
Sandeep Baynes <sandeep@nelson.management>
2023-01-16 23:30:37 +0530

    Added function code to update or get the tenant property.

--- index_local.js
+++ index_local.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const listtenants = require('./lambda_src/listtenants');
+const managetenantprops = require('./lambda_src/manageTenantProps');
 
 const port = 8000;
 var app = express();
@@ -10,6 +11,30 @@ app.get('/api/tenant/listtenants', function (req, res) {
     console.log(`List tenants function accessed with data`);
     console.log(req.query);
     listtenants.handler({ queryStringParameters: req.query || {} }).then(function (ret) {
+        res.statusCode = 200;
+        res.send(JSON.parse(ret.body));
+    }).catch(function (err) {
+        console.log(err);
+    })
+});
+
+app.get('/api/tenant/:tenantname/:environmentname', function (req, res) {
+    console.log(`Get tenant function accessed with data`);
+    console.log({ tenantname: req.params.tenantname, environmentname: req.params.environmentname });
+    managetenantprops.handler({ pathParameters: req.params, httpMethod: 'GET' }).then(function (ret) {
+        res.statusCode = ret.statusCode;
+        res.contentType = res.type('application/json');
+        res.send(JSON.parse(ret.body));
+    }).catch(function (err) {
+        console.log(err);
+    })
+});
+
+app.post('/api/tenant/:tenantname/:environmentname', function (req, res) {
+    console.log(`Update tenant function accessed with data`);
+    console.log({ tenantname: req.params.tenantname, environmentname: req.params.environmentname });
+    console.log(req.body);
+    managetenantprops.handler({ pathParameters: req.params, body: JSON.stringify(req.body), httpMethod: 'POST' }).then(function (ret) {
         res.statusCode = 200;
         res.send(JSON.parse(ret.body));
     }).catch(function (err) {
---
+++ lambda_src/manageTenantProps.js
@@ -0,0 +1,82 @@
+/*
+    Method updates or retrieves the tenant/environment properties based on the HTTP method
+*/
+const { S3Client, GetObjectCommand, PutObjectCommand } = require('@aws-sdk/client-s3');
+
+const s3Props = { region: process.env.ENV_REGION }
+if (process.env.LOCAL) {
+    s3Props.credentials = {
+        accessKeyId: process.env.ACCESSKEY,
+        secretAccessKey: process.env.SECRETKEY
+    };
+}
+const s3Client = new S3Client(s3Props);
+
+exports.handler = async (event) => {
+    const { environmentname, tenantname } = event.pathParameters;
+    var response = {};
+    if (environmentname == null || tenantname == null) {
+        return {
+            statusCode: 400,
+            body: JSON.stringify({
+                error: "Missing parameters"
+            })
+        };
+    }
+
+    if (event.httpMethod == 'GET') {
+        try {
+            //Get the tenant property from S3
+            const getObjectCmd = new GetObjectCommand({
+                Bucket: process.env.TENANT_PROPS_BUCKET,
+                Key: `${tenantname}/${environmentname}/config.json`
+            });
+            const s3Response = await s3Client.send(getObjectCmd);
+            const data = await s3Response.Body.transformToString();
+            response = JSON.parse(data);
+        }
+        catch (err) {
+            return {
+                statusCode: 500,
+                body: JSON.stringify({
+                    message: err.message
+                })
+            };
+        }
+    }
+    else if (event.httpMethod == 'POST') {
+        try {
+            if(event.body == null) {
+                return {
+                    statusCode: 400,
+                    body: JSON.stringify({
+                        error: "Missing body"
+                    })
+                }; 
+            }
+            //Update the tenant property to S3
+            var buf = Buffer.from(event.body);
+            const putObjectCmd = new PutObjectCommand({
+                Bucket: process.env.TENANT_PROPS_BUCKET,
+                Key: `${tenantname}/${environmentname}/config.json`,
+                ContentType: 'application/json',
+                Body: buf
+            });
+            await s3Client.send(putObjectCmd);
+            response.message = "Config updated successfully";
+        }
+        catch (err) {
+            return {
+                statusCode: 500,
+                body: JSON.stringify({
+                    message: err.message
+                })
+            };
+        }
+    }
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify(response)
+    };
+}
