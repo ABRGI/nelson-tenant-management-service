3060456641f32292896565d395d3a930e0670e90
Anh Nguyen Quang <nquanganh@tma.com.vn>
2026-02-04 15:37:07 +0700

    Support allowSAP

--- lambda_src/listtenants.js
+++ lambda_src/listtenants.js
@@ -86,6 +86,7 @@ exports.handler = async (event) => {
                         sitehost: environment.sitehost,
                         type: environment.type,
                         managementsitehost: environment.managementsitehost,
+                        allowSAP: environment.allowSAP,
                         hotels: environment.hotels.filter(hotel => hotelids.indexOf(hotel.id) > -1)
                     };
                     if (newEnvironment.hotels && newEnvironment.hotels.length > 0) {
@@ -131,6 +132,7 @@ function cleanUpExpiredHotelsInTenant(tenant) {
             sitehost: environment.sitehost,
             type: environment.type,
             managementsitehost: environment.managementsitehost,
+            allowSAP: environment.allowSAP,
             hotels: environment.hotels.filter(hotel => hotel.available)
         });
     });
